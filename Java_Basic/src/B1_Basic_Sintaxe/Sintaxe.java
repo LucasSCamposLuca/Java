8330673e2cee290fe76e07b9f4b120c6c3a26a59
LucasSCamposLuca <lucassouza104840@gmail.com>
2025-06-06 06:18:40 -0300

    Adicinando projetos local

---
+++ Java_Basic/src/B1_Basic_Sintaxe/Sintaxe.java
@@ -0,0 +1,15 @@
+// package: tem a função principal de orgranizar as classes em pacotes para evitar possíveis conflitos
+package B1_Basic_Sintaxe;
+// Treinando Java!!!
+
+// public: é um modificador de acesso que permite que a classe, método ou atributo possa ser acessada de qualquer parte do programa.
+// class: é como se fosse um molde para criação de novos objetos por exemplo carros, a partir da classe carro é possível criar deversos carros como amarock, frontier e etc atravéz dessa classe colocando atributos(característica) e métodos(ações).
+public class Sintaxe {
+    // static: significa que o método ou atributo pertence a classe.
+    // vodi: o método não retorna nenhum valor
+    // main: método principal para a execução
+
+    public static void main (String [] args){
+        System.out.println(args[1]  );
+    }
+}
